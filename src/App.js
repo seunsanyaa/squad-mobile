2ff88fdab51dbbaed841adc1802c0f193a0bfb0f
seunsanyaa <seunsanyaa@gmail.com>
2021-12-26 20:46:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+          <div className="menu-icon">
+              <img src="data:image/svg+xml,%3Csvg width='32' height='36' viewBox='0 0 32 36' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4 27H28M4 9H28H4ZM4 18H28H4Z' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E%0A"/>
+
+          </div>
+
+          <div className="firstSection">
+
+      <img className="squad" src="data:image/svg+xml, %3Csvg width='340' height='214' viewBox='0 0 340 214' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M34.23 133.28C34.23 131.66 34.05 130.31 33.69 129.23C33.33 128.15 32.7 126.89 31.8 125.45L3.72 78.47C2.28 75.95 1.38 73.79 1.02 71.99C0.84 70.19 0.75 68.21 0.75 66.05V16.91C0.75 11.15 2.37 7.00998 5.61 4.48998C8.85 1.78999 13.35 0.439988 19.11 0.439988H37.2C42.96 0.439988 47.46 1.78999 50.7 4.48998C53.94 7.00998 55.56 11.15 55.56 16.91V64.16H34.23V19.61H22.08V64.16C22.08 65.78 22.26 67.22 22.62 68.48C23.16 69.56 23.79 70.73 24.51 71.99L50.97 116.27C52.05 117.89 52.86 119.33 53.4 120.59C53.94 121.85 54.39 123.11 54.75 124.37C55.11 125.63 55.29 127.07 55.29 128.69C55.47 130.31 55.56 132.29 55.56 134.63V180.53C55.56 186.29 53.94 190.52 50.7 193.22C47.46 195.74 42.96 197 37.2 197H19.11C13.35 197 8.85 195.74 5.61 193.22C2.37 190.52 0.75 186.29 0.75 180.53V133.28H22.08V177.83H34.23V133.28ZM88.211 197C82.811 196.82 78.491 195.2 75.251 192.14C72.011 188.9 70.391 184.4 70.391 178.64V18.8C70.391 13.4 72.011 8.98999 75.251 5.56999C78.671 2.14999 83.171 0.439988 88.751 0.439988H106.841C112.421 0.439988 116.831 2.14999 120.071 5.56999C123.491 8.98999 125.201 13.4 125.201 18.8V178.64C125.201 184.4 123.581 188.9 120.341 192.14C117.101 195.2 112.781 196.82 107.381 197V213.2H88.211V197ZM104.141 177.83V19.61H91.721V177.83H104.141ZM196.129 0.439988V178.64C196.129 184.4 194.419 188.9 190.999 192.14C187.759 195.38 183.349 197 177.769 197H159.679C153.919 197 149.419 195.38 146.179 192.14C142.939 188.9 141.319 184.4 141.319 178.64V0.439988H162.649V177.83H174.799V0.439988H196.129ZM212.246 17.99C212.246 13.13 213.956 8.98999 217.376 5.56999C220.796 2.14999 224.936 0.439988 229.796 0.439988H250.046C254.906 0.439988 259.046 2.14999 262.466 5.56999C265.886 8.98999 267.596 13.13 267.596 17.99V197H246.266V108.17H233.576V197H212.246V17.99ZM246.266 19.61H233.576V88.73H246.266V19.61ZM317.722 19.61H305.032V177.83H317.722V19.61ZM339.052 179.72C339.052 184.4 337.342 188.45 333.922 191.87C330.502 195.29 326.452 197 321.772 197H283.702V0.439988H321.772C326.452 0.439988 330.502 2.14999 333.922 5.56999C337.342 8.98999 339.052 13.13 339.052 17.99V179.72Z' fill='white'/%3E%3C/svg%3E"/>
+<img className="girlImage" src="https://res.cloudinary.com/seunsanyaa/image/upload/v1640542330/portraitsfordrawing_1_gjhul9.png"/>
+          </div>
+
+
+          <div className="secondSection">
+              <img width={342} className="girlImage" src="https://res.cloudinary.com/seunsanyaa/image/upload/v1640542644/20_Cool_Bald_Fade_Haircuts_for_Men_1_puoykr.png"/>
+<div className="textarea">
+    <h3 className="name"> KUNLE</h3>
+
+    <p>
+        Kunle is the creative director of SQUAD. His goal is to bring Nigerian fashion into a worldwide spotlight. <br/>
+       <span className="padding">
+        SQUAD is the pioneer agency transforming fashion in Nigeria and bringing togther a roast of independent spirits that challenge the status-quo.
+       <br/>
+        Our network of creative pieple have helped hundreds of designers and together, we are constantly evolving.
+       </span>
+    </p>
+
+</div>
+          </div>
+
       </header>
     </div>
   );
